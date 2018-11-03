82c8299b159c706fa3d5966fb41f24042257e1c5
Victor Yap <victor@vicyap.com>
2018-11-03 00:34:36 -0500

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/vicyap/vectra_api_client',
-    version='0.2.0',
+    version='0.3.0',
     zip_safe=False,
 )
