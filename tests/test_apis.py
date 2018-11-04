379ea31dc5a6ccb83d9b70ce4f289c815e37ea9e
Muhaimin Ahsan <leroux@fezrev.com>
2018-11-04 07:30:30 -0600

    Do some detect swagger. Nicer API.

---
+++ tests/test_apis.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `vectra_api_client` package."""
+
+import pytest
+import requests
+
+
+from vectra_api_client import apis
+
+
+def test_clients_v1(mocker):
+    mocker.patch('requests.Session.request')
+
+    client = apis.v1(host='https://localhost', username='', password='')
+    client.session.get('/whatever')
+    assert requests.Session.request.called
+
+
+def test_clients_v2(mocker):
+    mocker.patch('requests.Session.request')
+
+    client = apis.v2(host='https://localhost', token='')
+    client.session.get('/whatever')
+    assert requests.Session.request.called
