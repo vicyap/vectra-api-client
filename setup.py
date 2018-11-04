f3dd06b777414575619ae8a4ff8e5add4cbc4d7f
Victor Yap <victor@vicyap.com>
2018-11-04 01:42:12 -0500

    Bump version: 0.5.0 → 0.6.0

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/vicyap/vectra_api_client',
-    version='0.5.0',
+    version='0.6.0',
     zip_safe=False,
 )
--- vectra_api_client/__init__.py
+++ vectra_api_client/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 """Top-level package for Vectra API Client."""
 
-__version__ = '0.5.0'
+__version__ = '0.6.0'
